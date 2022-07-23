b2bf8d9130d4ebcc5f9675a8009c1a762fba4256
Sidiki-del <you@example.com>
2022-07-23 20:51:40 +0000

    Back File Added

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,25 @@
-<x-app-layout>
-    
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin.css');
+</head>
+
+<body>
+    <div class="container-scroller">
+        <!-- partial:partials/_sidebar.html -->
+        @include('admin.sidebar');
+
+        <!-- partial -->
+        @include('admin.navbar');
+
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('admin.script');
+
+    <!-- End custom js for this page -->
+</body>
+
+</html>
