7340053ee7eec452067ab52b111cef56b270f453
Sidiki-del <you@example.com>
2022-09-09 20:05:41 +0000

    From Cart To Order

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 
 class HomeController extends Controller
 {
@@ -96,6 +97,41 @@ class HomeController extends Controller
         $cart->delete();
 
         return redirect()->back();
+    }
+
+    public function cash_order(){
+        $user = Auth::user();
+        $userid = $user->id;
+        $data = cart::where('user_id', '=', $userid)->get();
+
+        foreach($data as $data)
+        {
+            $order = new order;
+
+            $order->name = $data -> name;
+            $order->email = $data -> email;
+            $order->phone = $data -> phone;
+            $order->user_id = $data -> user_id;
+            $order->product_title = $data -> product_title;
+            $order->address = $data -> address;
+            $order->price = $data -> price;
+            $order->quantity = $data -> quantity;
+            $order->image = $data -> image;
+            $order->product_id = $data -> product_id;
+            $order->payment_status = 'Cash On Delivery';
+            $order->delivery_status = 'Processing';
+
+            $order -> save();
+
+            $cart_id = $data -> id;
+            $cart = cart::find($cart_id);
+            $cart->delete();
+        }
+
+            return redirect()->back()->with('message', 'Move To Order Table');
+
+
+        
     }
     
 }
--- resources/views/home/show_cart.blade.php
+++ resources/views/home/show_cart.blade.php
@@ -67,6 +67,14 @@
 
         <!-- end slider section -->
         <!-- why section -->
+
+        @if(session()->has('message'))
+        <div class='alert alert-success'>
+            <button type='button' class='close' data-dismiss='alert' aria-hidden='true'>x</button>
+            {{session()->get('message')}}
+        </div>
+        @endif
+
         <div class='center'>
 
             <table>
@@ -97,6 +105,12 @@
             </table>
             <div class="total_deg">
                 <h1>Total Price: {{$totalprice}} CFA</h1>
+            </div>
+
+            <div>
+                <h1 style="font-size: 25px; padding-bottom: 20px;">Proceed To Order</h1>
+                <a href="{{url('cash_order')}}" class='btn btn-danger'>Cash On Delivery</a>
+                <a href="" class='btn btn-danger'>Pay Using Card</a>
             </div>
 
         </div>
